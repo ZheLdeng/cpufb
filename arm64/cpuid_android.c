902823865af05f55ca7953ec23f3a34872567ae2
VictorQAQ <859897180@qq.com>
2024-05-15 15:56:32 +0800

    add build android part

---
+++ arm64/cpuid_android.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<stdbool.h>
+
+#define MAX_FEATURES 100
+#define MAX_FEATURE_LENGTH 1000
+
+bool find(const char *str, char (*features)[MAX_FEATURE_LENGTH], int count) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(str, features[i]) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int getCPUFeatures(char (*features)[MAX_FEATURE_LENGTH], int max_features) {
+    char command[] = "cat /proc/cpuinfo | grep Features | head -n1";
+    FILE* pipe = popen(command, "r");
+    int count = 0;
+
+    if (!pipe) {
+        perror("popen() failed");
+        return -1;
+    }
+
+    // 读取命令输出并存储到数组中
+    while (fgets(features[count], MAX_FEATURE_LENGTH, pipe)) {
+        // 移除换行符
+        features[count][strlen(features[count]) - 1] = '\0';
+
+        // 使用冒号进行分割
+        char *feature = strtok(features[count], ":");
+        feature = strtok(NULL, ":"); // 忽略冒号前的部分
+
+        // 使用空格进行分割
+        char *token = strtok(feature, " ");
+        while (token != NULL) {
+            // 存储特性项到数组中
+            strcpy(features[count], token);
+            count++;
+            if (count >= max_features) {
+                fprintf(stderr, "Too many features\n");
+                break;
+            }
+            token = strtok(NULL, " ");
+        }
+    }
+
+    pclose(pipe);
+    return count;
+}
+
+int main() {
+    char features[MAX_FEATURES][MAX_FEATURE_LENGTH];
+
+    int count = getCPUFeatures(features, MAX_FEATURES);
+    // for (int i = 0; i < count; i++) {
+    //     printf("%s\n", features[i]);
+    // }
+    if(find("asimd",features,count)){
+        printf("_ASIMD_\n");
+    }
+    if(find("asimddp",features,count)){
+        printf("_ASIMD_DP_\n");
+    }
+    if(find("asimdhp",features,count)){
+        printf("_ASIMD_HP_\n");
+    }
+    if(find("i8mm",features,count)){
+        printf("_I8MM_\n");
+    }
+    if(find("bf16",features,count)){
+        printf("_BF16_\n");
+    }
+    printf("_LDP_\n");     
+
+    return 0;
+}
---
+++ build_android.sh
@@ -0,0 +1,55 @@
+set -x
+SRC=arm64
+ASM=$SRC/asm
+COMM=common
+BUILD_DIR=build_dir
+
+
+# Get system information
+OS=$(uname -s)
+CPU=$(uname -m)
+
+CXX=g++
+CC=gcc
+CFLAG=
+
+echo "Operating System  : $OS"
+echo "CPU Architecture  : $CPU"
+
+# Cross Compile 
+if [[ "$CPU" == "x86_64" || "$CPU" == "i686" ]]; then
+    CXX=aarch64-linux-gnu-g++
+    CC=aarch64-linux-gnu-gcc
+    CFLAG=-DCROSS_COMPILE
+fi
+
+
+
+# make directory
+if [ -d "$BUILD_DIR" ]; then
+    rm -rf $BUILD_DIR/*
+else
+    mkdir $BUILD_DIR
+fi
+
+# build common tools
+$CXX  -g -O2 -c $COMM/table.cpp -o $BUILD_DIR/table.o
+$CXX  -g -O2 -pthread -c $COMM/thread_pool.cpp -o $BUILD_DIR/thread_pool.o
+
+# gen benchmark macro according to cpuid feature
+$CC $SRC/cpuid_android.c -o $BUILD_DIR/cpuid $CFLAG -static
+adb push $BUILD_DIR/cpuid  /data/local/tmp
+SIMD_MACRO=" "
+SIMD_OBJ=" "
+#-march=native 交叉编译不用加#
+for SIMD in `adb shell  /data/local/tmp/cpuid`;
+do
+    SIMD_MACRO="$SIMD_MACRO-D$SIMD "
+    SIMD_OBJ="$SIMD_OBJ$BUILD_DIR/$SIMD.o "
+    # $CC -mtune=generic -g -c $ASM/$SIMD.S -o $BUILD_DIR/$SIMD.o
+    as -g -mcpu=all -c $ASM/$SIMD.S -o $BUILD_DIR/$SIMD.o
+done
+# compile cpufp
+$CXX -g -O2 -I$COMM $SIMD_MACRO -c $SRC/cpufp.cpp -o $BUILD_DIR/cpufp.o 
+$CXX -g -O2 -z noexecstack -pthread -static -o cpufp $BUILD_DIR/cpufp.o $BUILD_DIR/thread_pool.o $BUILD_DIR/table.o $SIMD_OBJ 
+set +x
